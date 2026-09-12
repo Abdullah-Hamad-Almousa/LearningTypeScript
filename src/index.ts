9e4dfa946fea095626abfa5f51f4c353a9fbf0ef
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-12 19:41:36 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+let age : number = 26
+let firstName : string = 'Abdulalh'
+let isFictional : boolean
+
+age = ++age
+firstName = 'Almousa'
+isFictional = false
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+planet = 'Saturn'
+moons = 145
+isLarge = true
+
+//Null and undefined
+
+let something : null
+let thing : undefined
+
+something = null
+something = null
+
+thing = undefined
