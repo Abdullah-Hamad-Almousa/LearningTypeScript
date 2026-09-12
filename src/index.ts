6c84bcaec6a9e09a9e0bbc673f9337458f889ef5
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-12 22:31:27 +0300

    Type Guards

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
-//Union Types
-
-let id: number | string
-
-id = 1
-id = "one"
-
-let email: string | null = null
-
-email = 'bowser@mario-world.devil'
-email = null
+//Type Guards
 
 type Id = number | string
-let idTwo: Id
 
-idTwo = 'sadhjkbasda'
-idTwo = 5
+function swapIdType(id: Id){
 
-//Union Types pitfall
+    if (typeof id === 'string'){
+        //Can use string methods
+        return parseInt(id)
+    } else {
+        //Can use numbers methods and properties
+        return id.toString()
+    }
 
-function swap(id: Id): Id {
-
-// can only use props and methods common to
-// both number and string types
-// parseInt(id) -- > not allowed
-
-    //parseInt(id)
-
-    return id
 }
 
-swap('5')
+const idOne = swapIdType(1)
+const idTwo = swapIdType('2')
+
+console.log(idOne)
+console.log(idTwo)
+
+interface User {
+    type: 'user'
+    username: string
+    email: string
+    id: Id
+}
+interface Person {
+    type: 'person'
+    firstname: string
+    age: number
+    id: Id
+}
+
+function logDetails(value: User | Person): void {
+    if (value.type === 'user') {
+        console.log(value.email, value.username)
+    }
+    if (value.type === 'person') {
+        console.log(value.firstname, value.age)
+    }
+}
