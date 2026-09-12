d888d3125e70f2cefc9acf58811e069b2990e293
Abdullah almousa <abdullahmlwork@gmail.com>
2026-09-12 21:34:42 +0300

    any type

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,24 @@
-//Functions
+//Any type
 
-function addTwoNumbers(a : number, b : number): number {
-    return a + b
+let age: any
+
+age = 30
+age = false
+
+let title
+
+title = 25
+title = 'index'
+
+let things: any[] = ['hello', true, 27, null]
+
+things.push({id:123})
+
+function addTogether(value: any): any {
+    return value + value
 }
 
-const subTwoNumbers = (a: number, b: number): number => {
-    return a - b
-}
+const resultOne = addTogether('Hello')
+const resultTwo = addTogether(3)
 
-addTwoNumbers(3, 9)
-
-subTwoNumbers(10, 7)
-
-function addAllNum(items: number[]): void{
-    const total = items.reduce((a, c)=> a + c, 0)
-    console.log(total)
-}
-
-addAllNum([5, 7, 9, 11, 3, 2, 1])
-
-console.log("Hello Abdullah")
-
-function formatGreeting(name: string, greeting: string){
-    return `${greeting}, ${name}`
-}
-
-const result = formatGreeting('Abdullah', 'The master')
-
-console.log(result)
+console.log(resultOne, "\t", resultTwo)
